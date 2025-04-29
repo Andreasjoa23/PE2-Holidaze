707c5a306378d3c4e8b395e4e49684050470483d
AndreasAUG23F <andabr00198@stud.noroff.no>
2025-04-29 12:54:52 +0200

    fixed bug in venues.ts

--- src/api/venues.ts
+++ src/api/venues.ts
@@ -6,9 +6,21 @@ export async function getAllVenues() {
 }
  */
 
-import apiClient from "./apiClient";
+
+
+
+/* import apiClient from "./apiClient";
 
 export async function getAllVenues() {
   const response = await apiClient.get("/holidaze/venues");
   return response.data.data; // <-- viktig: hent ut kun venues-array
+} */
+
+
+
+  import apiClient from "./apiClient";
+
+export async function getAllVenues() {
+  const response = await apiClient.get("/holidaze/venues");
+  return response.data; // Returner hele objektet
 }
